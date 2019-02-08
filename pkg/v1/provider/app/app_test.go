55d3550da0405e232164e31406f6433dd39223bb
Peter van den Broek <peter.vandenbroek@isaac.nl>
2019-02-08 10:27:00 +0100

    PIMSAA-119: Added tests for the app provider.

---
+++ pkg/v1/provider/app/app_test.go
@@ -0,0 +1,34 @@
+package app
+
+import (
+	"github.azc.ext.hp.com/fitstation-hp/lib-fs-core-go/pkg/v1/version"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"os"
+	"testing"
+)
+
+func TestApp(t *testing.T) {
+	RegisterFailHandlerWithT(t, Fail)
+	RunSpecs(t, "App provider test")
+}
+
+var _ = Describe("App provider", func() {
+	name := "TestApp"
+	_ = os.Setenv("APP_NAME", name)
+	// Proper testing for version is done in the core package.
+	version.BuildString = "0.13.90 a722bdb 2018-01-09T22:32:37+01:00 go version go1.11 linux/amd64"
+
+	It("Returns the app parameters", func() {
+		c := NewConfigFromEnv()
+		p := New(c)
+
+		err := p.Init()
+		Expect(err).ToNot(HaveOccurred())
+
+		Expect(p.Name()).To(Equal(name))
+
+		Expect(p.Version()).ToNot(BeNil())
+		Expect(p.Version().String()).To(Equal(version.BuildString))
+	})
+})
